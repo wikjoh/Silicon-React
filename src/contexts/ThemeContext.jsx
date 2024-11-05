16cecc6453c38a69ab0f8631f70d01c957a5ae74
wikai <80284758+wikai@users.noreply.github.com>
2024-11-05 14:52:32 +0100

    Update darkmode to use useEffect for updating localstorage

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -11,16 +11,19 @@ const ThemeProvider = ({ children }) => {
     } else return matchMedia('(prefers-color-scheme: dark)').matches
   }
 
-
+  
   const [darkmode, setDarkmode] = useState(getDarkmode)
 
 
   const toggleDarkmode = () => {
-    const newValue = !darkmode
-    setDarkmode(newValue)
-    localStorage.setItem('darkmode', JSON.stringify(newValue))
+    setDarkmode(prevState => !prevState)
   }
 
+
+  useEffect(() => {
+    localStorage.setItem('darkmode', JSON.stringify(darkmode))
+  }, [darkmode])
+  
 
   return (
     <ThemeContext.Provider value={{ darkmode, toggleDarkmode }}>
