3978577e46cb159b10e7678deae2c98e17d9c19c
wikai <80284758+wikai@users.noreply.github.com>
2024-11-07 18:39:19 +0100

    Add name attribute to faq details elements to auto-close previous faq item when opening another one

--- src/features/section-faqcontact/components/FAQItem.jsx
+++ src/features/section-faqcontact/components/FAQItem.jsx
@@ -2,15 +2,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const FAQItem = ({ title, content }) => {
   return (
-    <details>
-        <summary className="question">
-          <span>{ title }</span>
-          <label className="btn-chevron chevron-faq"><FontAwesomeIcon icon="fa-chevron-down" /></label>
-        </summary>
-        <div className="answer">
-          <p>{ content }</p>
-        </div>
-      </details>
+    <details name="faq">
+      <summary className="question">
+        <span>{ title }</span>
+        <label className="btn-chevron chevron-faq"><FontAwesomeIcon icon="fa-chevron-down" /></label>
+      </summary>
+      <div className="answer">
+        <p>{ content }</p>
+      </div>
+    </details>
   )
 }
 export default FAQItem
