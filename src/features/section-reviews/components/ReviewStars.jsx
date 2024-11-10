864f2183b674bb049d3be1b1e3082cd0cdec63b4
wikai <80284758+wikai@users.noreply.github.com>
2024-11-10 01:40:06 +0100

    Fix missing key prop on review/testimonial stars

--- src/features/section-reviews/components/ReviewStars.jsx
+++ src/features/section-reviews/components/ReviewStars.jsx
@@ -1,12 +1,13 @@
 const ReviewStars = ({ starRating }) => {
   return (
     <>
+    {/* Items won't change once rendered, so using index as key prop should be fine */}
     {Array.from({ length: starRating }).map((_, i) => (
-      <img src="/icons/rating-star.svg" alt="" />
+      <img key={i} src="/icons/rating-star.svg" alt="" />
     ))}
 
     {Array.from({ length: (5 - starRating) }).map((_, i) => (
-      <img src="/icons/rating-star-empty.svg" alt="" />
+      <img key={i} src="/icons/rating-star-empty.svg" alt="" />
     ))}
     </>
   )
