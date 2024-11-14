41b50c2ac94378803345637dc65869ded014802f
wikai <80284758+wikai@users.noreply.github.com>
2024-11-14 23:53:49 +0100

    Add tablet/desktop mode for section-contact

--- src/features/section-contact/SectionContact.jsx
+++ src/features/section-contact/SectionContact.jsx
@@ -5,27 +5,29 @@ import styles from './SectionContact.module.css'
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1>Contact Us</h1>
-      <div className={styles.emailCard}>
-        <ContactCard
-          title='Email us'
-          text='Please feel free to drop us a line. We will respond as soon as possible.'
-          url='#'
-          urlText='Leave a message'
-          icon='/icons/emailicon.svg'
-          />
-      </div>
-      <div className={styles.careersCard}>
-        <ContactCard
-          title='Careers'
-          text='Sit ac ipsum leo lorem magna nunc mattis maecenas non vestibulum.'
-          url='#'
-          urlText='Send an application'
-          icon='/icons/careeraddicon.svg'
-          />
+      <div className={styles.textContent}>
+        <h1>Contact Us</h1>
+        <div className={styles.emailCard}>
+          <ContactCard
+            title='Email us'
+            text='Please feel free to drop us a line. We will respond as soon as possible.'
+            url='#'
+            urlText='Leave a message'
+            icon='/icons/emailicon.svg'
+            />
+        </div>
+        <div className={styles.careersCard}>
+          <ContactCard
+            title='Careers'
+            text='Sit ac ipsum leo lorem magna nunc mattis maecenas non vestibulum.'
+            url='#'
+            urlText='Send an application'
+            icon='/icons/careeraddicon.svg'
+            />
+        </div>
       </div>
 
-      <div className="consultForm" style={{marginTop: "4rem", marginBottom: "4rem"}}>
+      <div className={styles.consultationForm}>
         <ConsultationForm/>
       </div>
 
