fe846e1abb74cade5d7c34cc267c65a80bb551af
wikai <80284758+wikai@users.noreply.github.com>
2024-11-16 16:19:07 +0100

    Make addresscard margins responsive

--- src/features/section-address/Address.jsx
+++ src/features/section-address/Address.jsx
@@ -6,11 +6,11 @@ const Address = () => {
   return (
     <div className={styles.container}>
       <Map />
-      <div className={styles.addressesContainer}>
-        <div style={{marginTop: "3rem"}}>
+      <div className={styles.cardContainer}>
+        <div className={styles.card1}>
           <AddressCard  title='Medical Center 1' address='4517 Washington Ave. Manchester, Kentucky 39495' phoneNumber='(406) 555-0120' monFriHours='9:00 am – 22:00 am' satSunHours='9:00 am – 20:00 am' />
         </div>
-        <div style={{marginTop: "3rem"}}>
+        <div className={styles.card2}>
           <AddressCard  title='Medical Center 2' address='2464 Royal Ln. Mesa, New Jersey 45463' phoneNumber='(406) 544-0123' monFriHours='9:00 am – 22:00 am' satSunHours='9:00 am – 20:00 am' />
         </div>
       </div>
