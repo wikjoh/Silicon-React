405d9f1cec2730ddd3ae03a3f463baaff8033c43
wikai <80284758+wikai@users.noreply.github.com>
2024-11-17 17:19:37 +0100

    Delete unused how-does-it-work code, adjust opacities

--- src/features/section-how/components/HowImages.jsx
+++ src/features/section-how/components/HowImages.jsx
@@ -8,51 +8,21 @@ const HowImages = ({ imgOrder }) => {
     else return 'desktop'
   }
 
-  if (1 === 1) {
-    return (
-      <div className={styles.images} data-device={getDeviceType()} aria-label="A set of images showcasing some features of the app.">
-        <div className={`${styles.imgBgContainer}`}>
-          <img src={`/images/${getDeviceType()}/howdoesitwork-bg.png`} alt="Budget and spending." />
-        </div>
-        <div className={`${styles.imgContainer}`} data-imgorder={imgOrder[0]} data-device={getDeviceType()}>
-          <img src={`/images/${getDeviceType()}/howdoesitwork-left.png`} alt="Budget and spending." />
-        </div>
-        <div className={`${styles.imgContainer}`} data-imgorder={imgOrder[1]} data-device={getDeviceType()}>
-          <img src={`/images/${getDeviceType()}/howdoesitwork-center.png`} alt="Card transactions." />
-        </div>
-        <div className={`${styles.imgContainer}`} data-imgorder={imgOrder[2]} data-device={getDeviceType()}>
-          <img src={`/images/${getDeviceType()}/howdoesitwork-right.png`} alt="Money transfers." />
-        </div>
+  return (
+    <div className={styles.images} data-device={getDeviceType()} aria-label="A set of images showcasing some features of the app.">
+      <div className={`${styles.imgBgContainer}`}>
+        <img src={`/images/${getDeviceType()}/howdoesitwork-bg.png`} alt="Budget and spending." />
       </div>
-    )
-  } else if (1===0) {
-
-  } else {
-      return (
-        <div className="images-container" aria-label="A set of images showcasing some features of the app.">
-        <div className="img tablet left">
-          <img src="/images/tablet/howdoesitwork-left.png" alt="Card transactions." />
-        </div>
-        <div className="img desktop left">
-          <img src="/images/desktop/howdoesitwork-left.png" alt="Budget and spending." />
-        </div>
-        <div className="img mobile center">
-          <img src="/images/mobile/howdoesitwork.png" alt="Money transfers." />
-        </div>
-        <div className="img tablet center">
-          <img src="/images/tablet/howdoesitwork-center.png" alt="Money tranfers." />
-        </div>
-        <div className="img desktop center">
-          <img src="/images/desktop/howdoesitwork-center.png" alt="Card transactions." />
-        </div>
-        <div className="img tablet right">
-          <img src="/images/tablet/howdoesitwork-right.png" alt="Money transfer interface." />
-        </div>
-        <div className="img desktop right">
-          <img src="/images/desktop/howdoesitwork-right.png" alt="Money transfers." />
-        </div>
+      <div className={`${styles.imgContainer}`} data-imgorder={imgOrder[0]} data-device={getDeviceType()}>
+        <img src={`/images/${getDeviceType()}/howdoesitwork-left.png`} alt="Budget and spending." />
       </div>
-    )
-  }
+      <div className={`${styles.imgContainer}`} data-imgorder={imgOrder[1]} data-device={getDeviceType()}>
+        <img src={`/images/${getDeviceType()}/howdoesitwork-center.png`} alt="Card transactions." />
+      </div>
+      <div className={`${styles.imgContainer}`} data-imgorder={imgOrder[2]} data-device={getDeviceType()}>
+        <img src={`/images/${getDeviceType()}/howdoesitwork-right.png`} alt="Money transfers." />
+      </div>
+    </div>
+  )
 }
 export default HowImages
