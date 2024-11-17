ad95b970098fd496654da280ce6eb724bcf7f138
wikai <80284758+wikai@users.noreply.github.com>
2024-11-17 16:56:32 +0100

    Add tablet responsivity for carousel

--- src/features/section-how/components/HowImages.jsx
+++ src/features/section-how/components/HowImages.jsx
@@ -2,23 +2,31 @@ import styles from './HowImages.module.css'
 
 const HowImages = ({ imgOrder }) => {
 
-  if (1===1) {
+  const getDeviceType = () => {
+    if (window.matchMedia('(max-width: 767px)').matches) return 'mobile'
+    else if (window.matchMedia('(max-width: 1399px)').matches) return 'tablet'
+    else return 'desktop'
+  }
+
+  if (1 === 1) {
     return (
-      <div className={styles.images} aria-label="A set of images showcasing some features of the app.">
-        <div className={`${styles.imgBgContainer} desktop`}>
-          <img src="/images/desktop/howdoesitwork-bg.png" alt="Budget and spending." />
+      <div className={styles.images} data-device={getDeviceType()} aria-label="A set of images showcasing some features of the app.">
+        <div className={`${styles.imgBgContainer}`}>
+          <img src={`/images/${getDeviceType()}/howdoesitwork-bg.png`} alt="Budget and spending." />
         </div>
-        <div className={`${styles.imgContainer} desktop`} data-imgorder={imgOrder[0]}>
-          <img src="/images/desktop/howdoesitwork-left.png" alt="Budget and spending." />
+        <div className={`${styles.imgContainer}`} data-imgorder={imgOrder[0]} data-device={getDeviceType()}>
+          <img src={`/images/${getDeviceType()}/howdoesitwork-left.png`} alt="Budget and spending." />
         </div>
-        <div className={`${styles.imgContainer} desktop`} data-imgorder={imgOrder[1]}>
-          <img src="/images/desktop/howdoesitwork-center.png" alt="Card transactions." />
+        <div className={`${styles.imgContainer}`} data-imgorder={imgOrder[1]} data-device={getDeviceType()}>
+          <img src={`/images/${getDeviceType()}/howdoesitwork-center.png`} alt="Card transactions." />
         </div>
-        <div className={`${styles.imgContainer} desktop`} data-imgorder={imgOrder[2]}>
-          <img src="/images/desktop/howdoesitwork-right.png" alt="Money transfers." />
+        <div className={`${styles.imgContainer}`} data-imgorder={imgOrder[2]} data-device={getDeviceType()}>
+          <img src={`/images/${getDeviceType()}/howdoesitwork-right.png`} alt="Money transfers." />
         </div>
       </div>
     )
+  } else if (1===0) {
+
   } else {
       return (
         <div className="images-container" aria-label="A set of images showcasing some features of the app.">
